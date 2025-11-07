12a4a9e0e149bbf8dc7e368a5b62f6c85ed77e1d
MPelland-programming <maxime.pelland.programming@gmail.com>
2025-11-06 19:58:18 -0500

    Update generate initial population
    Switched from lists to numpy arrays

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+#parameters this should be transferred to the init part of a python class
+n = 100 #number of observations
+n_best = 30 #number of best solutions to keep
+p_parent = 0.13 #proportion of population to be selected as parents
+p_good = 0.8#proportion of good parents from the selected parents
+
+n_parent = n_best*p_parent//1 #number of parents to select
+n_good = n_parent*p_good//1   #number of good parents to select
+n_bad = -1*(n_parent*(1-p_good)//-1)  #number of bad parents to select
+
 def generate_initial_population(n_best,n,k=2):
   """
   Generate n_best individuals
-  Returns: list of individuals, each
+  Returns: a numpy array of shape pop. size x k dim x n observations
   """
   popu = []
-  for i in range(n_best):
-    tind = rng.random(size=(k,n)).tolist()
-    popu.append(tind)
+  popu = rng.random(size=(n_best,k,n))
 
   return popu
+
+
